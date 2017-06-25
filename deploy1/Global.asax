9a43e5ebf13bec92948a90050d01fd9cb50b594f
Jayakrishnan-ks <Jayakrishnan_KS@dell.com>
2017-06-25 10:43:00 +0530

    deploy commit

---
+++ deploy1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="deploy1.Global" Language="C#" %>
---
+++ deploy1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deploy1.Startup))]
+namespace deploy1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
